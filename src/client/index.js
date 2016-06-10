f1dddf20a318404f0934f50e2a5c2c0d2c0c7d30
istinson <ian.c.stinson@gmail.com>
2016-06-09 19:14:33 -0700

    [feat] render initial input field

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './container/app';
+
+
+//app extends component (class?)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
