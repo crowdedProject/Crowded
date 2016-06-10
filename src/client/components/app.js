1a77458cd6e2d0d2c22e2fb9da51846d05587a60
istinson <ian.c.stinson@gmail.com>
2016-06-10 16:24:26 -0700

    [feat] create search input functionality

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 
 import SearchBar from '../containers/search-bar';
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cafes: []
+    };
+  }
+
+  // componentWillMount() {
+  //   this.cafeSearch('thisisadummycoffeeshop');
+  // }
+
+  cafeSearch(term) {
+    console.log(term)
+    //post to server at endpoint that server recognizes
+
+    //receive cafe data from server
+    
+  }
+
   render() {
+    //should debounce yelp api call
+    const cafeSearch = _.debounce((term) => {
+      this.cafeSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={cafeSearch}/>
       </div>
     );
   }
 }
+
+export default App;
