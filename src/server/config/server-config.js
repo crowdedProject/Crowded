1df3c25415e2036c4a52b98ca040549d7af7be14
Adam Behrens <behrens.adam@gmail.com>
2016-06-11 16:19:58 -0700

    [fix] correct relative path in server

--- src/server/config/server-config.js
+++ src/server/config/server-config.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static('./../../../dist'));
+app.use(express.static(`${__dirname}./../../../dist`));
 app.use(bodyParser.urlencoded({'extended':'true'}));          
 app.use(bodyParser.json());                                     
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
