3c2464655c7699a8dd72a1486a7be12426013879
istinson <ian.c.stinson@gmail.com>
2016-06-10 20:20:04 -0700

    [chore] add comments

--- src/client/components/cafe-list.js
+++ src/client/components/cafe-list.js
@@ -1,12 +1,15 @@
-//I may want to make this a container
-//instead of a component.
+//This will become a container
+//once redux comes in to play
 import React from 'react';
 
 import Cafe from './cafe';
 
+//Add REDUX to determine whether we render
+//Neighborhoods, Preferences, or Cafes
+
 const CafeList = (props) => {
-  //DummyArray in place of props
-  //since it's not yet functional
+  //Dummy array is assigned to props since 
+  //it's not yet integrated with server.
   props = [1, 2, 3, 4, 5];
   const cafeArr = props.map((cafe) => {
     return (
