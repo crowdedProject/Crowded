7d1753c7a98fab96a274f319236cb8c11c94f34b
Bo <boyao2005@gmail.com>
2016-06-13 15:20:03 -0700

    [feat] implement server side yelpAPI call

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,14 @@
 const app = require('./config/server-config.js');
+const Yelp = require('../client/yelpApiCall/yelpSearch');
+
 
 let port = process.env.PORT || 8080;
 
 app.listen(port, function() {
 	console.log("server listening on port " + port);
+});
+
+app.post('/cafeResult', (req, res) => {
+	// console.log('serverPOST');
+	Yelp.yelpResult(req, res);
 });
