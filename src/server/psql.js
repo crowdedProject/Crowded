35f6355c9f7111fb6dd8bffa9be82046aa55a77b
Adam Behrens <behrens.adam@gmail.com>
2016-06-14 15:00:13 -0700

    [test] create shell tests for postgres

--- src/server/psql.js
+++ src/server/psql.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const connection = require('./config/db-connection.js');
 const db = process.env.DATABASE_URL || connection.local;
-const nData = require(__dirname + '/neighborhood.json');
+const nData = require(`${__dirname}/neighborhood.json`);
 
 if (!global.hasOwnProperty('pg')) {
   pg = (() => {
@@ -22,9 +22,9 @@ pg
   .then((err) => { console.log('connection successful');})
   .catch((err) => { console.log('unable to connect to database:', err);});
   
-const User = pg.import(__dirname + '/db-models/db-user');
-const Cafe = pg.import(__dirname + '/db-models/db-cafe');
-const Neighborhood = pg.import(__dirname + '/db-models/db-neighborhood');
+const User = pg.import(`${__dirname}/db-models/db-user`);
+const Cafe = pg.import(`${__dirname}/db-models/db-cafe`);
+const Neighborhood = pg.import(`${__dirname}/db-models/db-neighborhood`);
 
 pg.sync({force: true})
   .then(() => {
