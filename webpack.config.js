a02b6d40bdadb9face93517cd619e34bcf8fa035
Adam Behrens <behrens.adam@gmail.com>
2016-06-16 14:16:24 -0700

    Revert "Revert "[feat] build out reducer for cafe list page""

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 //look at docs and update to include html webpack! (this will allow you to delete the style folder from dist)
 
@@ -12,14 +12,14 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'crowdedCafe',
-      template: 'client/index.html',
-      filename: 'index.html'
-    }),
-    new ExtractTextPlugin('style.css')
-  ],
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'crowdedCafe',
+  //     template: 'client/index.html',
+  //     filename: 'index.html'
+  //   }),
+  //   new ExtractTextPlugin('style.css')
+  // ],
   module: {
     loaders: [{
       exclude: /node_modules/,
