237ac6ad3a3d7e6ae08f5ff65b8fa8cb5f5d0bae
Bo <boyao2005@gmail.com>
2016-06-23 17:29:15 -0700

    [feat] create user favor link

--- src/client/routes.js
+++ src/client/routes.js
@@ -6,12 +6,13 @@ import PrefList from './containers/pref-list';
 import CafeList from './containers/cafe-list';
 import NeighborhoodList from './containers/neighborhood-list';
 import Auth0UserLogin from './containers/auth0_userLogin';
-
+import UserFavorList from './containers/user-favorlist';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PrefList} />
     <Route path="/neighborhood" component={NeighborhoodList} />
     <Route path="/login" component={Auth0UserLogin} />
     <Route path="/cafes" component={CafeList} />
+    <Route path="/favorite" component={UserFavorList} />
   </Route>
 );
