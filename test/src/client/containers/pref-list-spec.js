a0ac52c257c00c7ebbafbab23c0d1f59b0e9b9ef
istinson <ian.c.stinson@gmail.com>
2016-07-02 09:38:56 -0700

    [dev] create pref-list specs

--- test/src/client/containers/pref-list-spec.js
+++ test/src/client/containers/pref-list-spec.js
@@ -1,20 +1,32 @@
-import { renderComponent, expect } from '../../../test_helper';
+import {renderComponent, expect} from '../../../test_helper';
 import PrefList from '../../../../src/client/containers/pref-list';
 
 describe('PrefList', () => {
   let component;
 
   beforeEach(() => {
-    const props = { comments: ['New Comment', 'Other New Comment'] };
-    component = renderComponent(PrefList, null, props);
+    //const props = { comments: ['New Comment', 'Other New Comment'] };
+    component = renderComponent(PrefList);
   });
 
-  it('shows an LI for each comment', () => {
-    expect(component.find('li').length).to.equal(2);
+  it('has the correct class', () => {
+    expect(component).to.have.class('PrefList');
   });
 
-  it('shows each comment that is provided', () => {
-    expect(component).to.contain('New Comment');
-    expect(component).to.contain('Other New Comment');
+  it('shows the search and submit buttons', () => {
+    expect(component).to.contain('Choose Neighborhood');
+    expect(component).to.contain('Find Cafes')
+  });
+
+  it('shows the 9 preference options', () => {
+    expect(component).to.contain('Coffee');
+    expect(component).to.contain('Ambiance');
+    expect(component).to.contain('Rating');
+    expect(component).to.contain('Seats');
+    expect(component).to.contain('Outlets');
+    expect(component).to.contain('Bathrooms');
+    expect(component).to.contain('Line');
+    expect(component).to.contain('Noise');
+    expect(component).to.contain('Price');
   });
 });
